249e7570d97aa9eec990512efcff2258331d14b4
adem.polat <adem.polat@partner.yildizholding.com.tr>
2024-05-01 00:47:21 +0300

    Initial commit

---
+++ app/src/main/java/com/example/waterbottlecanvas/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.example.waterbottlecanvas
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.waterbottlecanvas.ui.theme.WaterBottleCanvasTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WaterBottleCanvasTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    var usedAmount by remember {
+                        mutableIntStateOf(400)
+                    }
+
+                    val totalWaterAmount = remember {
+                        2400
+                    }
+
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        WaterBottle(totalWaterAmount = totalWaterAmount, unit = "ml", usedWaterAmount = usedAmount, modifier = Modifier.width(250.dp))
+                        Spacer(modifier = Modifier.height(20.dp))
+                        Text(text = "Total amount is : $totalWaterAmount")
+                        Button(onClick = {
+                            if (usedAmount<=2400){
+                                usedAmount += 200
+                            }
+                        }) {
+                            Text(text = "Drink")
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
